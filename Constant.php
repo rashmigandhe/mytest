2c3fe05a12af646ed267bb6186ce906673e631ce
Rashmi Gandhe <rgandhe@regard-solutions.com>
2011-10-12 19:20:52 +0530

    -a

--- Constant.php
+++ Constant.php
@@ -1,8 +1,13 @@
 <?php
-$host = '192.168.91.76';
-$username = 'root';
-$password = 'root';
-$database = 'Circa';
+//$host = '192.168.91.76';
+$host = '10.194.111.8';
+
+//$username = 'root';
+$username = 'user_29df75ea';
+//$password = 'root';
+$password = 'L48GuN-5(BzQLO';
+//$database = 'Circa';
+$database = 'db_29df75ea';
 $uploaddir = 'images/';
 $Radius = 0.50;
 ?>
