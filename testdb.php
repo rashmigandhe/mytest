df2cbcfbd4439216a10fe4d3859f58356b18384d
Rashmi Gandhe <rgandhe@regard-solutions.com>
2011-10-12 18:22:13 +0530

    testdb.php

---
+++ testdb.php
@@ -0,0 +1,14 @@
+<?php
+
+require("Constant.php");
+
+$con = mysql_connect($host,$username,$password );
+echo "con=$con<br>";
+if (!$con)
+  {
+	 die('Could not connect: ' . mysql_error());
+  }
+
+$val = mysql_select_db("Circa") or die(mysql_error()); 
+echo "val=$val<br>";
+?>
